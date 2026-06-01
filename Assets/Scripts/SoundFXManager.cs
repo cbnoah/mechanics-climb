7e6c50e483bcf87c8b9703539230c7a3beb689bf
cnoah <noah.charrinbourrat@ynov.com>
2026-06-01 19:46:25 +0200

    feat: sound manager added

---
+++ Assets/Scripts/SoundFXManager.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class SoundFXManager : MovementBehavior
+    {
+        public static SoundFXManager Instance;
+
+        [SerializeField] private AudioSource soundFXObject;
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+        }
+
+        public void PlaySound(AudioClip audioClip, Transform spawnTransform, float volume)
+        {
+            AudioSource audioSource = Instantiate(soundFXObject, spawnTransform.position, Quaternion.identity);
+
+            audioSource.clip = audioClip;
+
+            audioSource.volume = volume;
+
+            audioSource.Play();
+
+            float clipLenght = audioSource.clip.length;
+
+
+            Destroy(audioSource.gameObject, clipLenght);
+        }
+    }
+}
