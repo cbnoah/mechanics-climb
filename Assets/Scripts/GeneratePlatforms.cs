1fc798e85e90ff4a11e8b9df879296730ef33573
Noah CHARRIN--BOURRAT <noah.charrinbourrat@ynov.com>
2026-06-01 14:41:54 +0200

    feat: generate platform script added

---
+++ Assets/Scripts/GeneratePlatforms.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratePlatforms : MonoBehaviour
+{
+    [SerializeField] private GameObject platformPrefab;
+    [SerializeField] private GameObject camera;
+    private List<GameObject> platforms;
+
+    private void Start()
+    {
+        for (var i = 0; i < 10; i++)
+        {
+            platforms.Add(Instantiate(platformPrefab, new Vector3(0, i * 2.0f, 0), Quaternion.identity));
+        }
+    }
+
+    private void verifyIsInCameraVison()
+    {
+        foreach (GameObject platform in platforms)
+        {
+            if (platform.transform.position.y < camera.transform.position.y)
+            {
+                
+            }
+        }
+    }
+}
