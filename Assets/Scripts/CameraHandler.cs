351a4477b00533867a618206f97550d8cfd4c7b8
Noah CHARRIN--BOURRAT <noah.charrinbourrat@ynov.com>
2026-06-01 14:41:23 +0200

    feat: camera handling for keeping the camera's center at the user's highest point added

---
+++ Assets/Scripts/CameraHandler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CameraHandler : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    
+    private void Start()
+    {
+    }
+
+    private void FixedUpdate()
+    {
+        if (transform.position.y < player.transform.position.y)
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * player.transform.position.y);
+        }
+    }
+}
