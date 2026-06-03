293485eecf8d5c5ffe0f357a662912139185196e
Noah CHARRIN--BOURRAT <noah.charrinbourrat@ynov.com>
2026-06-03 11:42:45 +0200

    feat: score handler now save the player's score as PlayerPrefs

--- Assets/Scripts/ScoreHandler.cs
+++ Assets/Scripts/ScoreHandler.cs
@@ -4,11 +4,43 @@ using UnityEngine;
 public class ScoreHandler : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI scoreText;
-    [SerializeField] private Camera _camera;
+    [SerializeField] private TextMeshProUGUI highScoreText;
+    private new Camera camera;
+    private int _score;
+    private int _highScore;
+    private string _fileName;
+    private GameManager _gameManager;
+
+    public int GetScore()
+    {
+        return _score;
+    }
+
+    private void Awake()
+    {
+        _highScore = PlayerPrefs.GetInt("HighScore");
+    }
+
+    private void Start()
+    {
+        camera = Camera.main;
+        _gameManager = GameManager.Instance;
+        _gameManager.OnGameOver.AddListener(SaveScore);
+    }
 
     private void Update()
     {
-        var score = (int)_camera.transform.position.y;
-        scoreText.text = "Score : " + score;
+        _score = (int)camera.transform.position.y;
+        scoreText.text = "Score : " + _score;
+        highScoreText.text = "High Score : " + _highScore;
+    }
+
+    private void SaveScore()
+    {
+        PlayerPrefs.SetInt("Score", _score);
+        if (PlayerPrefs.GetInt("HighScore") < _score)
+        {
+            PlayerPrefs.SetInt("HighScore", _score);
+        }
     }
 }
