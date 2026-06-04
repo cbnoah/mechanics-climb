75770064d84e114a32dbc06a002e3dd6057de9f7
cnoah <noah.charrinbourrat@ynov.com>
2026-06-04 23:30:39 +0200

    feat: operations have been optimised

--- Assets/Scripts/BounceBehaviour.cs
+++ Assets/Scripts/BounceBehaviour.cs
@@ -3,17 +3,17 @@ using UnityEngine;
 public class BounceBehaviour : MonoBehaviour
 {
     [SerializeField] float bounceForce = 15f;
-    private void OnCollisionEnter2D(Collision2D other) {
-        if (other.gameObject.CompareTag("Player")) {
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
             MovementBehavior movement = other.gameObject.GetComponent<MovementBehavior>();
             Rigidbody2D rb = other.gameObject.GetComponent<Rigidbody2D>();
-            if (movement != null && movement.IsFalling() && rb != null) {
-                //rb.linearVelocity = new Vector2(rb.linearVelocity.x, bounceForce);
-                rb.linearVelocity = Vector2.up * bounceForce;
-                Debug.Log("Bounce! New velocity: " + rb.linearVelocity);
-                movement.SetFalling(false);
-            }
+            if (movement == null || !movement.IsFalling() || rb == null) return;
+            rb.linearVelocity = Vector2.up * bounceForce;
+            Debug.Log("Bounce! New velocity: " + rb.linearVelocity);
+            movement.SetFalling(false, gameObject.ToString());
         }
     }
-    
 }
