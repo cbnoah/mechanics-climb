87e58efbb65bb092d6bd40aa3cd4521348c49da5
cnoah <noah.charrinbourrat@ynov.com>
2026-06-04 23:35:09 +0200

    feat: improve camera follow behavior with smooth vertical movement

--- Assets/Scripts/CameraHandler.cs
+++ Assets/Scripts/CameraHandler.cs
@@ -3,16 +3,15 @@
 public class CameraHandler : MonoBehaviour
 {
     [SerializeField] private GameObject player;
-    
-    private void Start()
-    {
-    }
+    [SerializeField] private float speed = 5f;
 
-    private void LateUpdate()
+    private void Update()
     {
         if (transform.position.y < player.transform.position.y)
         {
-            transform.Translate(Vector3.up * Time.deltaTime * player.transform.position.y);
+            float targetY = player.transform.position.y;
+            float newY = Mathf.Lerp(transform.position.y, targetY, speed * Time.deltaTime);
+            transform.position = new Vector3(transform.position.x, newY, transform.position.z);
         }
     }
 }
