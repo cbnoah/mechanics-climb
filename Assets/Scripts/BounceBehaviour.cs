bde45c9332c2b757fce6705818eb4c23ac1d135c
cnoah <noah.charrinbourrat@ynov.com>
2026-06-14 23:12:21 +0200

    feat: bounce sound and platform breaking behaviour has been added to bounce behaviour

--- Assets/Scripts/BounceBehaviour.cs
+++ Assets/Scripts/BounceBehaviour.cs
@@ -1,8 +1,10 @@
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class BounceBehaviour : MonoBehaviour
 {
     [SerializeField] float bounceForce = 15f;
+    [SerializeField] [CanBeNull] AudioClip[] breakableBounceSounds;
 
     private void OnCollisionEnter2D(Collision2D other)
     {
@@ -14,6 +16,12 @@ public class BounceBehaviour : MonoBehaviour
             rb.linearVelocity = Vector2.up * bounceForce;
             Debug.Log("Bounce! New velocity: " + rb.linearVelocity);
             movement.SetFalling(false, gameObject.ToString());
+        }
+
+        if (gameObject.ToString().Contains("Breakable"))
+        {
+            SoundFXManager.Instance.PlaySound(breakableBounceSounds[Random.Range(0, breakableBounceSounds.Length-1)], transform, 0.1f);
+            gameObject.SetActive(false);
         }
     }
 }
